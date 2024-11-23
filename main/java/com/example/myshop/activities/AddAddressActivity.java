bb92914230cb543078f9cc40fac79515bf340b4b
Ali Tariq <alitariq69399@gmail.com>
2024-11-23 20:40:38 +0500

    Add files via upload

---
+++ main/java/com/example/myshop/activities/AddAddressActivity.java
@@ -0,0 +1,115 @@
+package com.example.myshop.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myshop.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddAddressActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    EditText name, address, city, postalCode, phoneNumber;
+    AppCompatButton addAddressBtn;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_address);
+        // Change status bar color
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.textHeading));
+
+        toolbar = findViewById(R.id.add_address_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.getNavigationIcon().setTint(getResources().getColor(R.color.white));
+        toolbar.setTitleTextAppearance(this, R.style.MainToolbarTitle);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        auth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
+
+        name = findViewById(R.id.ad_name);
+        address = findViewById(R.id.ad_address);
+        city = findViewById(R.id.ad_city);
+        postalCode = findViewById(R.id.ad_code);
+        phoneNumber = findViewById(R.id.ad_phone);
+        addAddressBtn = findViewById(R.id.ad_add_address);
+
+        addAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = name.getText().toString();
+                String userCity = city.getText().toString();
+                String userAddress = address.getText().toString();
+                String userCode = postalCode.getText().toString();
+                String userNumber = phoneNumber.getText().toString();
+
+                String final_address = "";
+
+                if (!userName.isEmpty()){
+                    final_address+=userName;
+                }
+                if (!userAddress.isEmpty()){
+                    final_address+=userAddress;
+                }
+                if (!userCity.isEmpty()){
+                    final_address+=userCity;
+                }
+                if (!userCode.isEmpty()){
+                    final_address+=userCode;
+                }
+                if (!userNumber.isEmpty()){
+                    final_address+=userNumber;
+                }
+                if (!userName.isEmpty() && !userCity.isEmpty() && !userAddress.isEmpty() && !userCode.isEmpty() && !userNumber.isEmpty()){
+
+                    Map<String,String> map = new HashMap<>();
+                    map.put("userAddress",final_address);
+
+                    firestore.collection("CurrentUser").document(auth.getCurrentUser().getUid())
+                            .collection("Address").add(map).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentReference> task) {
+                                    if (task.isSuccessful()){
+                                        Toast.makeText(AddAddressActivity.this, "Address Added", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(AddAddressActivity.this, DetailedActivity.class));
+                                        finish();
+                                    }
+                                }
+                            });
+                }else {
+                    Toast.makeText(AddAddressActivity.this, "Kindly fill all field!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ main/java/com/example/myshop/activities/ShowAllActivity.java
@@ -0,0 +1,189 @@
+package com.example.myshop.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.myshop.R;
+import com.example.myshop.adapters.ShowAllAdapter;
+import com.example.myshop.models.ShowAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowAllActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ShowAllAdapter showAllAdapter;
+    List<ShowAllModel> showAllModelList;
+    Toolbar toolbar;
+
+    FirebaseFirestore firestore;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_all);
+
+        String type = getIntent().getStringExtra("type");
+
+        // Change status bar color
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(R.color.textHeading));
+
+        firestore = FirebaseFirestore.getInstance();
+        toolbar = findViewById(R.id.show_all_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.getNavigationIcon().setTint(getResources().getColor(R.color.white));
+        toolbar.setTitleTextAppearance(this, R.style.MainToolbarTitle);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        recyclerView = findViewById(R.id.show_all_rec);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        showAllModelList = new ArrayList<>();
+        showAllAdapter = new ShowAllAdapter(this, showAllModelList);
+        recyclerView.setAdapter(showAllAdapter);
+
+
+        if (type == null || type.isEmpty()){
+            firestore.collection("ShowAll")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+        if (type != null && type.equalsIgnoreCase("men")){
+            firestore.collection("ShowAll").whereEqualTo("type","men")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+        if (type != null && type.equalsIgnoreCase("women")){
+            firestore.collection("ShowAll").whereEqualTo("type","women")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+        if (type != null && type.equalsIgnoreCase("camera")){
+            firestore.collection("ShowAll").whereEqualTo("type","camera")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+        if (type != null && type.equalsIgnoreCase("shoes")){
+            firestore.collection("ShowAll").whereEqualTo("type","shoes")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+        if (type != null && type.equalsIgnoreCase("kids")){
+            firestore.collection("ShowAll").whereEqualTo("type","kids")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+        if (type != null && type.equalsIgnoreCase("watch")){
+            firestore.collection("ShowAll").whereEqualTo("type","watch")
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                            if (task.isSuccessful()){
+                                for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                    ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                    showAllModelList.add(showAllModel);
+                                    showAllAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+}
